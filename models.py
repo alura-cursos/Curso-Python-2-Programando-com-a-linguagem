782000e758b74a7927c9f1384fde20a7d13a7ff1
Andre Chaves <andrechavesg@gmail.com>
2017-12-26 09:30:14 -0200

    capitulo9

--- models.py
+++ models.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 
 class Perfil(object):
-    'Classse para moldar perfis de usuarios'
+    'Classe padrão para perfis de usuários'
 
     def __init__(self, nome, telefone, empresa):
         self.nome = nome
         self.telefone = telefone
         self.empresa = empresa
+        self.__curtidas = 0
 
-    def imprimir(self):
-    	print 'Nome %s, Telefone %s, Empresa %s' % (self.nome, self.telefone, self.empresa)
+        def imprimir(self):
+            print "Nome : %s, Telefone: %s, Empresa: %s, Curtidas: %s" % (self.nome, self.telefone, self.empresa, self.__curtidas)   
 
+        def curtir(self):
+            self.__curtidas+=1
+
+        def obter_curtidas(self):
+            return self.__curtidas
+            
 class Data(object):
    def __init__(self, dia, mes, ano):
       self.dia = dia
